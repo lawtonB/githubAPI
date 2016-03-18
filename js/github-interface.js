5eff3d76d10211267b4aabb208e1fb6ea49342e9
lawton browning <lawton.a.browning@gmail.com>
2016-03-18 10:00:44 -0700

    enable console to grab user info from github

--- js/github-interface.js
+++ js/github-interface.js
@@ -6,16 +6,23 @@ apiKey = "f73a42fce5f2b99f6d20b15a90b0011bb3ec40b7";
 
 $(document).ready(function() {
   $("#getUser").click(function(){
+
     var user = $("#userName").val();
     $(".showUser").text("the users name is " + user + ".");
-  });
-});
 
-
-  exports.getRepos = function(){
-    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    $.get("https://api.github.com/users/" + user + "?access_token=" + apiKey).then(function(response){
       console.log(response);
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
-  };
+  });
+});
+
+
+  // exports.getRepos = function(){
+  //   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+  //     console.log(response);
+  //   }).fail(function(error){
+  //     console.log(error.responseJSON.message);
+  //   });
+  // };
