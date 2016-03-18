8b1361d4f3fdc35c1f39849c7f4ecf0d049bd3e2
lawton browning <lawton.a.browning@gmail.com>
2016-03-18 13:21:19 -0700

    add readme and extra get pathname tolist all repos

--- js/getRepos.js
+++ js/getRepos.js
@@ -1,25 +1,23 @@
 var apiKey = require('./../.env').apiKey;
-apiKey = "f73a42fce5f2b99f6d20b15a90b0011bb3ec40b7";
+
 var userInfo = require('./../js/userInfo.js').userInfo;
 var userRepo = require('./../js/userRepo.js').userRepo;
 var clearInfo = require('./../js/clear-user.js').clearInfo;
 
 exports.getRepos = function(user){
-  // var user = $("#userName").val();
+
   clearInfo();
 
   $.get('https://api.github.com/users/' + user + "?access_token=" + apiKey).then(function(response){
     userInfo(response);
-    // console.log(gitHubUser);
-    // console.log(gitHubUser.email);
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 
-  $.get('https://api.github.com/users/' + user + "/repos?access_token" + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + user + "/repos?access_token" + + apiKey + "&per_page=1000" ).then(function(response){
     userRepo(response);
-    // console.log(gitHubUser);
-    // console.log(gitHubUser.email);
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
--- js/user-interface.js
+++ js/user-interface.js
@@ -1,7 +1,3 @@
-// var apiKey = require('./../.env').apiKey;
-//
-// apiKey = "f73a42fce5f2b99f6d20b15a90b0011bb3ec40b7";
-
 var getRepos = require('./../js/getRepos.js').getRepos;
 
 $(document).ready(function() {
@@ -9,11 +5,3 @@ $(document).ready(function() {
     getRepos($("#userNameInput").val());
   });
 });
-
-
-
-    // $.get("https://api.github.com/users/" + user + "?access_token=" + apiKey).then(function(response){
-    //   console.log(response);
-    // }).fail(function(error){
-    //   console.log(error.responseJSON.message);
-    // });
